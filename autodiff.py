42133d84bb9ddf8dec72365039fdc5c93fb0732d
okarthikb <86470305+okarthikb@users.noreply.github.com>
2022-02-09 09:01:41 +0530

    Update autodiff.py

--- autodiff.py
+++ autodiff.py
@@ -1,88 +1,91 @@
-# y = f(x1 ... xn)
-# dL / dxi = (dL / dy) * (dy / dxi)
-# where f is some primitive operation
-# primitive ops: +, -, *, /, **, log, min, max
-# above is all that's needed for scalar auto diff
-
-# f(x1 ... xn) returns a Var
-# whose children are x1 ... xn, and op is remembered
-
-
 from math import log
 
 
-f = {
-    "add": lambda a, b: a + b, 
-    "sub": lambda a, b: a - b, 
-    "mul": lambda a, b: a * b, 
-    "div": lambda a, b: a / b, 
-    "exp": lambda a, b: a ** b
-}
+def ddiv(i, a, b):
+    return -a.val / (b.val * b.val) if i == 1 else 1 / b.val
 
-df = {
-    "add": lambda *_: 1, 
-    "sub": lambda i, *_: -1 if i == 1 else 1, 
-    "mul": lambda i, a, b: a if i == 1 else b, 
-    "div": lambda i, a, b: -a / (b * b) if i == 1 else 1 / b, 
-    "exp": lambda i, a, b: b * a ** (b - 1) if i == 0 else log(a) * a ** b
-}
+
+def dpow(i, a, b):
+  if i == 1:
+    return log(a.val) * a.val ** b.val  
+  else:
+    return b.val * a.val ** (b.val - 1)
 
 
 class Var:
-    def __init__(self, val, args=None, x=None, requires_grad=False, fn=None):
-        self.val = val
-        self.grad = 0
-        self.requires_grad = requires_grad
-        self.fn = fn
-        self.args = args
-        self.x = x
+  def __init__(self, val, xs=(None, None), reqgrad=False, gradfn=lambda *_: 1):
+    if isinstance(val, Var):
+      raise TypeError("val must not be Var")
+    self.val = val
+    self.xs = xs
+    self.grad = None
+    self.reqgrad = reqgrad
+    self.gradfn = gradfn
 
-    def __repr__(self):
-        fn = f", fn={self.fn})" if self.requires_grad else ")"
-        return f"Var({self.val}" + fn
+  def __repr__(self):
+    return f"Var({self.val}" + (f", reqgrad={self.reqgrad})" if self.reqgrad else ')')
 
-    def par(self, fn, *args):
-        x = []
-        requires_grad = False
-        for arg in args:
-            x.append(arg.val)
-            requires_grad |= arg.requires_grad
-        return Var(f[fn](*x), args, x, requires_grad, fn)
+  def par(self, val, other, gradfn):
+    return Var(val, (self, other), self.reqgrad | other.reqgrad, gradfn)
 
-    def __add__(self, b):
-        return self.par("add", self, b)
+  def __mul__(self, other):
+    other = other if isinstance(other, Var) else Var(other)
+    return self.par(self.val * other.val, other, lambda i, a, b: a.val if i == 1 else b.val)
 
-    def __sub__(self, b):
-        return self.par("sub", self, b)
+  def __neg__(self):
+    return self * -1
 
-    def __mul__(self, b):
-        return self.par("mul", self, b)
+  def __add__(self, other):
+    other = other if isinstance(other, Var) else Var(other)
+    return self.par(self.val + other.val, other, lambda *_: 1)
 
-    def __truediv__(self, b):
-        return self.par("div", self, b)
+  def __sub__(self, other):
+    other = other if isinstance(other, Var) else Var(other)
+    return self.par(self.val - other.val, other, lambda i, *_: -1 if i == 1 else 1)
 
-    def __neg__(self):
-        return self.par("mul", self, -1)
+  def __truediv__(self, other):
+    other = other if isinstance(other, Var) else Var(other)
+    return self.par(self.val / other.val, other, ddiv)
 
-    def __pow__(self, b):
-        return self.par("exp", self, b)
+  def __pow__(self, other):
+    other = other if isinstance(other, Var) else Var(other)
+    return self.par(self.val ** other.val, other, dpow)
 
-    def __lt__(self, b):
-        return self.val < b.val
+  def log(self):
+    return self.par(log(self.val), Var(None), gradfn=lambda _, x, __: 1 / x.val)
 
-    def __gt__(self, b):
-        return self.val > b.val
+  def __radd__(self, other):
+    return self + other
 
-    def __eq__(self, b):
-        return self.val == b.val
+  def __rsub__(self, other):
+    return -self + other
 
-    def _backward_(self):
-        if self.args:
-            for i, arg in enumerate(self.args):
-                if arg.requires_grad:
-                    arg.grad += self.grad * df[self.fn](i, *self.x)
-                    arg._backward_()
+  def __rmul__(self, other):
+    return self * other
 
-    def backward(self):
-        self.grad = 1
-        self._backward_()
+  def __rtruediv__(self, other):
+    return other * self ** -1
+
+  def __rpow__(self, other):
+    other = other if isinstance(other, Var) else Var(other)
+    return other ** self
+
+  def _backward_(self):
+    for i, x in enumerate(self.xs):
+      if x and x.reqgrad:
+        dx = self.grad * self.gradfn(i, *self.xs)
+        x.grad = dx if x.grad is None else x.grad + dx
+        x._backward_()
+
+  def backward(self):
+    self.grad = Var(1)
+    self._backward_()
+
+
+def grad(f):
+  def wrap(*xs):
+    xs = tuple(Var(x.val, reqgrad=True) for x in xs)
+    y = f(*xs)
+    y.backward()
+    return tuple(x.grad for x in xs)
+  return wrap
