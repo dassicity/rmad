91bcc4cb436d73551885a1422dc42350bc0c6743
okarthikb <86470305+okarthikb@users.noreply.github.com>
2022-02-10 10:45:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,20 +2,24 @@ from autonn import *
 import matplotlib.pyplot as plt
 
 
-nn = NN((3, 4, 4), (sigmoid, relu), 1e-2)
+nn = NN((3, 5, 4), (relu, relu), 2e-2)  # initialize neural net
 
 losses = []
 
-print(f"before\n{nn([4, 2, 1, 8])}\n")
+# create a random classification dataset
+x = [[random() for _ in range(3)] for _ in range(4)]
+y = [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
 
-for _ in range(50):
-  loss = mse(nn([4, 2, 1, 8]), [0, 1, 1, 0])
+for _ in range(500):
+  loss = sum(mse(nn(a), b) for a, b in zip(x, y))  # compute mse loss
   losses.append(loss.val)
-  loss.backward()
-  nn.step()
+  loss.backward()  # backprop
+  nn.step()  # update parameters
 
-print(f"after\n{nn([4, 2, 1, 8])}")
+# print nn prediction and target vals
+print(f"nn(x): {[[z.val for z in nn(a)] for a in x]}\ny: {y}\n")
 
+# plot losses
 fig = plt.figure(figsize=(8, 8))
 plt.xlabel("epoch")
 plt.ylabel("loss")
