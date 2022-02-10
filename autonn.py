c9e5fe760195d0dedbb7c2030681b37daf4766c1
okarthikb <86470305+okarthikb@users.noreply.github.com>
2022-02-10 10:44:30 +0530

    Update autonn.py

--- autonn.py
+++ autonn.py
@@ -11,43 +11,51 @@ def sigmoid(x):
   return 1 / (1 + e ** (-x))
 
 
-def mul(w, x):
+def mul(w, x):  # matmul
   return [sum(a * b for a, b in zip(x, r)) for r in w]
 
 
 def randl(n):
-  return [Var(random(), reqdf=True) for _ in range(n)]
+  return [Var(random() / n, param=True) for _ in range(n)]
 
 
-def mse(p, y):
+def mse(p, y):  # mean square error
   return sum((b - a) ** 2 for a, b in zip(p, y))
 
 
 class NN:
   def __init__(self, dims, fs, lr):
-    self.dims = dims
+    self.dims = dims  # layer sizes
     self.w = [[randl(m) for _ in range(n)] for m, n in zip(dims[:-1], dims[1:])]
     self.b = [randl(n) for n in dims[1:]]
     self.l = [[0] * n for n in dims]
-    self.fs = fs
-    self.lr = lr
+    self.fs = fs  # activation functions
+    self.lr = lr  # learning rate
 
-  def zerograd(self):
+  def infer(self, param):  # disable grad during inference
+    param = not param
+    for i, (m, n) in enumerate(zip(self.dims[1:], self.dims[:-1])):
+      for r in range(m):
+        self.b[i][r].param = param
+        for c in range(n):
+          self.w[i][r][c].param = param
+
+  def zero(self):  # after update, zero gradients for next backprop
     for i, (m, n) in enumerate(zip(self.dims[1:], self.dims[:-1])):
       for r in range(m):
         self.b[i][r].grad = 0
         for c in range(n):
           self.w[i][r][c].grad = 0
 
-  def step(self):
+  def step(self):  # param update function
     for i, (m, n) in enumerate(zip(self.dims[1:], self.dims[:-1])):
       for r in range(m):
-        self.b[i][r].val -= self.lr * self.b[i][r].grad.val
+        self.b[i][r].val -= self.lr * self.b[i][r].grad
         for c in range(n):
-          self.w[i][r][c].val -= self.lr * self.w[i][r][c].grad.val
-    self.zerograd()
+          self.w[i][r][c].val -= self.lr * self.w[i][r][c].grad
+    self.zero()
 
-  def __call__(self, x):
+  def __call__(self, x):  # forward pass
     self.l[0] = x
     for i, (w, b, f) in enumerate(zip(self.w, self.b, self.fs)):
       self.l[i + 1] = [f(a) for a in [c + d for c, d in zip(mul(w, self.l[i]), b)]]
